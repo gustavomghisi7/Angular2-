78095e8d9d9dbdfffd8f8fd8e0c80dedf85e747a
Gustavo Martins Ghisi <guga-ghisi@hotmail.com>
2022-05-23 17:23:13 -0300

    finish review

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,6 +7,8 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'data-binding';
+
+  //Lifecycle hooks
   valor: number = 5;
   deletarCiclo: boolean = false;
 
--- src/app/ciclo/ciclo.component.ts
+++ src/app/ciclo/ciclo.component.ts
@@ -2,10 +2,10 @@ import {
   AfterContentChecked,
   AfterContentInit,
   AfterViewChecked,
-  AfterViewInit,
   Component,
   DoCheck,
   OnChanges,
+  OnInit,
   OnDestroy,
   Input
 } from '@angular/core';
@@ -16,11 +16,11 @@ import {
   styleUrls: ['./ciclo.component.css']
 })
 export class CicloComponent implements
+  OnInit,
   OnChanges,
   DoCheck,
   AfterContentInit,
   AfterContentChecked,
-  AfterViewInit,
   AfterViewChecked,
   OnDestroy {
 
@@ -30,36 +30,39 @@ export class CicloComponent implements
     console.log(this.constructor);
   }
 
+  // antes e quando o valor do property-binding é atualizado
   ngOnChanges(){
     console.log(this.ngOnChanges);
   };
 
+  // quando Component é inicializado
+  ngOnInit():void {
+    console.log(this.ngOnInit);
+  }
+  
+  // a cada ciclo de verificação de mudanças
   ngDoCheck(){
     console.log(this.ngDoCheck);
   };
   
+  // depois de inserir o conteúdo externo na view
   ngAfterContentInit(){
     console.log(this.ngAfterContentInit);
   };
   
+  // a cada verificação de conteúdo inserido
   ngAfterContentChecked(){
     console.log(this.ngAfterContentChecked);
   };
-
-  ngAfterViewInit(){
-    console.log(this.ngAfterViewInit);
-  };
   
+  // a cada verificação de conteúdo / conteúdo filho
   ngAfterViewChecked(){
     console.log(this.ngAfterViewChecked);
   };
-  
+
+  // antes da diretiva/component ser destruído
   ngOnDestroy(){
     console.log(this.ngOnDestroy);
   };
-
-  ngOnInit():void {
-    console.log(this.ngOnInit);
-  }
 
 }
