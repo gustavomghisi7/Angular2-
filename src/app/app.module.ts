f8e647d6af3fddacd94cbda4cf94d91e0f8ffffc
Gustavo Martins Ghisi <guga-ghisi@hotmail.com>
2022-05-24 12:09:08 -0300

    Angular CLI parte 1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,6 +9,7 @@ import { DataBindingComponent } from './data-binding/data-binding.component';
 import { InputPropertyComponent } from './input-property/input-property.component';
 import { OutputPropertyComponent } from './output-property/output-property.component';
 import { CicloComponent } from './ciclo/ciclo.component';
+import { CliComponent } from './cli/cli.component';
 
 @NgModule({
   declarations: [
@@ -17,7 +18,8 @@ import { CicloComponent } from './ciclo/ciclo.component';
     InputPropertyComponent,
     MeuFormComponent,
     OutputPropertyComponent,
-    CicloComponent
+    CicloComponent,
+    CliComponent
 
   ],
   imports: [
